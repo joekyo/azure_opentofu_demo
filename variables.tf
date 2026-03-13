9dcdb863b7544d1702692295a6290b3208436113
Nianzu Xu <xunianzu@gmail.com>
2026-03-13 20:50:10 +0800

    add var subscription id

--- variables.tf
+++ variables.tf
@@ -1,3 +1,8 @@
+variable "subscription_id" {
+  description = "Azure subscription ID"
+  type        = string
+}
+
 variable "location" {
   description = "Azure region to deploy resources into."
   type        = string
